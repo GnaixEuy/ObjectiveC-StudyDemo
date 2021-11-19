e0147269f056ffd9759c2f6ec0caadb89d143b0f
GnaixEuy <sgghcj@live.com>
2021-11-19 23:48:23 +0800

    :tada: init

--- Objective—C StudyDemo/main.m
+++ Objective—C StudyDemo/main.m
@@ -7,6 +7,20 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Person : NSObject
+{
+    int _age;
+    NSString* _name;
+    NSString* sex;
+}
+- (void) run;
+@end
+
+@implementation Person
+
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
