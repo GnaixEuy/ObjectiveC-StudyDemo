9c247e99b6274b6848deae380e55f76d6587a76f
GnaixEuy <sgghcj@live.com>
2021-11-20 00:01:32 +0800

    :sparkles: 学习了基本的对象语法

--- Objective—C StudyDemo/main.m
+++ Objective—C StudyDemo/main.m
@@ -8,23 +8,33 @@
 #import <Foundation/Foundation.h>
 
 @interface Person : NSObject
+//下方大括号内为类的成员对象，特征
 {
-    int _age;
+    @public //对外开放
     NSString* _name;
     NSString* sex;
+    int _age;
 }
+//下方为类方法声明，行为
 - (void) run;
 @end
 
 @implementation Person
 
+- (void)run{
+    NSLog(@"%@跑",_name);
+}
 
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
         NSLog(@"Hello, World!");
+        
+        Person* person = [Person new];
+        person->_name = @"小明";
+        [person run];
+        
     }
     return 0;
 }
