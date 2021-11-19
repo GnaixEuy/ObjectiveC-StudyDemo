6bb542a9ff21dd5fef113df096d70839e1486e55
GnaixEuy <sgghcj@live.com>
2021-11-19 23:42:19 +0800

    Initial Commit

---
+++ Objective—C StudyDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective—C StudyDemo
+//
+//  Created by 苏粤翔 on 2021/11/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
